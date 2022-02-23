4347b2510db7b7332e4c38d72873695f333ca01e
mh <matthar@web.de>
2022-02-23 20:04:53 +0100

    - Try Postgres database with adapted path (postgresql://... instead of postgres://...) - Use a heroku config variable as CSRF token   --> works well - db.create_all() commented out

--- main.py
+++ main.py
@@ -130,7 +130,7 @@ class Comment(db.Model):
     text = db.Column(db.Text, nullable=False)
 
 
-db.create_all()
+# db.create_all()
 
 
 # ----------------------------------------------------------------------------------------
