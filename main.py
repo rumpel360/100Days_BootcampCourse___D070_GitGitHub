4972679790f60cab3dd44bf9f36ca6c1d1030b23
mh <matthar@web.de>
2022-02-23 19:48:21 +0100

    - Try Postgres database again - Use a heroku config variable as CSRF token   --> works well

--- main.py
+++ main.py
@@ -43,8 +43,9 @@ gravatar = Gravatar(app,
 # ----------------------------------------------------------------------------------------
 # # CONNECT TO DB
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog_relations.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog_relations.db'
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///blog_relations.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
